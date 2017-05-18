f8491c497f2924337497565b7f436229b644619b
shubhamkansal <kansalshubham30@gmail.com>
2017-05-18 11:03:35 +0530

    Update log.py

--- log.py
+++ log.py
@@ -2,6 +2,7 @@
 
 import psycopg2
 
+
 def connect(database_name="news"):
     """Connect to the PostgreSQL database.  Returns a database connection."""
     try:
@@ -11,32 +12,36 @@ def connect(database_name="news"):
     except:
         print("Cannot connect to databse news")
 
+
 def articles():
-    db,c = connect()
+    db, c = connect()
     query = "select * from popular_articles limit 3"
     c.execute(query)
     article = c.fetchall()
     db.close()
-    for a in range(0,len(article),1):
+    for a in range(0, len(article), 1):
         print article[a][0] + ": " + str(article[a][1]) + ' views'
 
+
 def authors():
-    db,c = connect()
+    db, c = connect()
     query = "select * from third_view;"
     c.execute(query)
     author = c.fetchall()
     db.close()
-    for a in range(0,len(author),1):
+    for a in range(0, len(author), 1):
         print author[a][0] + ": " + str(author[a][1]) + ' views'
 
+
 def log():
-    db,c = connect()
+    db, c = connect()
     query = "select * from sixth_view where errors >= 1.0"
     c.execute(query)
     log = c.fetchall()
     db.close()
-    for a in range(0,len(log),1):
+    for a in range(0, len(log), 1):
         print str(log[a][0]) + " " + str(log[a][1]) + "%"
+
 
 if __name__ == '__main__':
     print "Most popular articles:\n"
